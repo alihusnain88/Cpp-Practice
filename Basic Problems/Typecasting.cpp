4fe2f397e5ce4d683240d2340fbf5e66c55bc4b4
Ali Husnain <122731330+alihusnain88@users.noreply.github.com>
2024-07-22 22:31:16 +0500

    Created Typecasting.cpp

---
+++ Basic Problems/Typecasting.cpp
@@ -0,0 +1,19 @@
+// Basic Typecasting
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Typecasting****************
+
+    int a = 2;
+    cout << "The integer value of a is :" << a       <<endl;
+    cout << "The float value of a is :" << (float)a  <<endl;
+
+    float b = 10.25;
+    cout << "The float value of b is :" << b         <<endl; // b = 10.25
+    cout << "The integer value of b is :" << (int)b  <<endl; // b = 10    
+
+    return 0;
+}
